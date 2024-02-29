578e5ef827f295328e129417b6a26b83c2e36ddb
hassan03042 <muhammadhassan6948@gmail.com>
2024-03-01 00:33:34 +0500

    code Updated and pending

---
+++ Chapter 5/app.js
@@ -0,0 +1,27 @@
+document.write ("<h3> Sum Of 3 + 5 is 8 </h3>" )
+
+
+//Q2 
+var num1 = 3
+var num2 = 5
+
+
+//Addition
+var addition = num1 + num2
+console.log (addition)
+
+//Subraction 
+var Subraction = num1 - num2
+console.log (Subraction)
+
+//Division
+var Division = num1 / num2
+console.log (Division)
+
+//Multiply 
+var Multiply = num1 * num2
+console.log (Multiply)
+
+//Modulus
+var Modulas = 5 % 3
+console.log (Modulas)
