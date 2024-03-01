540ed41ed84c27d88b06c5ec7df9137711e500ec
hassan03042 <muhammadhassan6948@gmail.com>
2024-03-02 00:49:16 +0500

    chapter 5 and google classroom code uploaded

---
+++ Classroom Questions/app.js
@@ -0,0 +1,32 @@
+//Q1
+var num1 = 5
+var num2 = 1
+
+//Addition
+var addition = num1 + num2
+console.log (addition)
+
+//Q2
+//Subraction
+var Subraction = num1 - num2
+console.log (Subraction)
+
+//Q3
+//Multiply
+var Multiply = num1 * num2
+console.log (Multiply)
+
+//Q4
+//Divide
+var Division = num1 / num2
+console.log (Division)
+
+//Q5
+//Modulas
+var Modulas = num1 % num2
+console.log (Modulas)
+
+//Q6
+//Square Of a Multiple Nmmbers
+var exponent = num1 ** 2
+console.log (exponent)
